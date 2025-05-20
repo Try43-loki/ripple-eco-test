a210b39494498fc97a301fdfb09f659a8f1a0dce
Eang Kimlong <eangkimlong@Eangs-MacBook-Pro.local>
2025-05-21 00:29:23 +0700

    start create component

---
+++ src/app/test/page.jsx
@@ -0,0 +1,13 @@
+import NavbarComponent from '@/components/NavbarComponent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <div>page</div>
+      <NavbarComponent/>
+    </>
+  )
+}
+
+export default page
