15e7756ac27d2329a06146c83a5254b91f0edc68
Eang Kimlong <eangkimlong@Eangs-MacBook-Pro.local>
2025-05-26 13:55:05 +0700

    init

---
+++ src/app/(user)/profile/_component/OwnFeedBackComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OwnFeedBackComponent = () => {
+  return (
+    <main>
+        
+    </main>
+  )
+}
+
+export default OwnFeedBackComponent
--- src/app/(user)/profile/page.jsx
+++ src/app/(user)/profile/page.jsx
@@ -1,7 +1,116 @@
+import HeroSectionComponent from "@/components/HeroSectionComponent";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { cn } from "@/lib/utils";
+import { ClipboardList, Clock, Mail, MapPin, MessageCircleQuestion, MessagesSquare, Phone, Sparkles, SquarePen } from "lucide-react";
 import React from "react";
 
 function ProfilePage() {
-  return <div>ProfilePage</div>;
+  return (
+    <main>
+      <HeroSectionComponent
+        text={" "}
+        description={" "}
+        showSearchBar={false}
+      />
+
+      <article className="flex items-end gap-x-10 rounded-[28px] mx-[150px] justify-end pr-10 py-7 relative -mt-28 bg-white shadow-lg">
+        <img
+          src="https://i.pinimg.com/736x/35/48/35/3548357337902e2d9d7a79b1a6a166bc.jpg"
+          alt="sakuke"
+          className="w-[170px] h-[170px] rounded-full absolute -top-10 left-30"
+        />
+        <div className="space-y-2">
+          <h2 className="text-[30px] text-green">Uchiha Sasuke</h2>
+          <p className="text-[20px] text-black">
+            Passionate about protecting the Earth and encouraging eco-friendly
+            habits.
+          </p>
+          <div className="flex items-center gap-x-[38px]">
+            <div className="flex items-center text-sub-info text-[#83948D] gap-x-[6px]">
+              <Phone className="w-[18px] h-[18px]" />
+              <p>012-333-334</p>
+            </div>
+            <div className="flex items-center text-sub-info text-[#83948D] gap-x-[6px]">
+              <MapPin className="w-[18px] h-[18px]" />
+              <p>Phnom Penh</p>
+            </div>
+            <div className="flex items-center text-sub-info text-[#83948D] gap-x-[6px]">
+              <Mail className="w-[18px] h-[18px]" />
+              <p>UNEP@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <Button className="bg-meduim-green hover:bg-green text-white px-[20px] py-[15px] rounded-[16px] text-[16px]">
+          <div className="flex gap-x-[6px] items-center">
+            <SquarePen className="w-[16px] h-[16px]" />
+            <p>Edit Profile</p>
+          </div>
+        </Button>
+      </article>
+      <section className="mt-10 mb-10 mx-[150px]">
+        <Tabs defaultValue="events" className="w-full ">
+          <TabsList className="flex flex-row gap-x-2 bg-white h-auto rounded-[14px] p-2 border-1 border-[#D8E2DD]">
+            <TabsTrigger
+              value="event-history"
+              className={cn(
+                "text-[16px] py-[10px] px-[16px] rounded-[14px] flex gap-x-[8px] items-center transition-colors",
+                "hover:bg-[#EDF0F3]",
+                "data-[state=active]:bg-meduim-green data-[state=active]:text-white"
+              )}
+            >
+              <Clock className="w-[18px] h-[18px]" />
+              <p>Event History</p>
+            </TabsTrigger>
+            <TabsTrigger
+              value="own-feedback"
+              className={cn(
+                "text-[16px] py-[10px] px-[16px] rounded-[14px] flex gap-x-[8px] items-center transition-colors",
+                "hover:bg-[#EDF0F3]",
+                "data-[state=active]:bg-meduim-green data-[state=active]:text-white"
+              )}
+            >
+              <MessagesSquare className="w-[18px] h-[18px]"/>
+              <p>Own Feedback</p>
+            </TabsTrigger>
+            <TabsTrigger
+              value="Discussion"
+              className={cn(
+                "text-[16px] py-[10px] px-[16px] rounded-[14px] flex gap-x-[8px] items-center transition-colors",
+                "hover:bg-[#EDF0F3]",
+                "data-[state=active]:bg-meduim-green data-[state=active]:text-white"
+              )}
+            >
+              <MessageCircleQuestion className="w-[18px] h-[18px]" />
+              <p>Discussion</p>
+            </TabsTrigger>
+            <TabsTrigger
+              value="take-action"
+              className={cn(
+                "text-[16px] py-[10px] px-[16px] rounded-[14px] flex gap-x-[8px] items-center transition-colors",
+                "hover:bg-[#EDF0F3]",
+                "data-[state=active]:bg-meduim-green data-[state=active]:text-white"
+              )}
+            >
+              <ClipboardList className="w-[18px] h-[18px]" />
+              <p>Take Action</p>
+            </TabsTrigger>
+            <TabsTrigger
+              value="Earned Badge"
+              className={cn(
+                "text-[16px] py-[10px] px-[16px] rounded-[14px] flex gap-x-[8px] items-center transition-colors",
+                "hover:bg-[#EDF0F3]",
+                "data-[state=active]:bg-meduim-green data-[state=active]:text-white"
+              )}
+            >
+              <Sparkles className="w-[18px] h-[18px]" />
+              <p>Earn Badge</p>
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </section>
+    </main>
+  );
 }
 
 export default ProfilePage;
