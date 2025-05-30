2ef4530bedb4f3d187aabbdacf3d309f6d332e3e
SreypheaThaong <thaong.sreyphea17@gmail.com>
2025-05-31 01:05:47 +0700

    updated code

--- src/app/(user)/take-action/[takeActionId]/page.jsx
+++ src/app/(user)/take-action/[takeActionId]/page.jsx
@@ -1,26 +1,31 @@
-import HeroSectionComponent from '@/components/HeroSectionComponent'
-import React from 'react'
+import HeroSectionComponent from "@/components/HeroSectionComponent";
+import React from "react";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbSeparator,
-  } from "@/components/ui/breadcrumb"
-import TakeActionDetailCard from '../_component/TakeActionDetailCard'
-import TakeActionFormComponent from '../_component/TakeActionFormComponent'
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import TakeActionDetailCard from "../_component/TakeActionDetailCard";
+import TakeActionFormComponent from "../_component/TakeActionFormComponent";
+import CreateTakeActionFormComponent from "@/components/CreateTakeActionFormComponent";
 const TakeActionDetailPage = () => {
   return (
     <main>
-        <HeroSectionComponent text={"TAKE ACTION"} description={"Share ideas, explore solutions, and connect with others driving environmental change."} showSearchBar={false} />
+      <HeroSectionComponent
+        text={"TAKE ACTION"}
+        description={
+          "Share ideas, explore solutions, and connect with others driving environmental change."
+        }
+        showSearchBar={false}
+      />
 
-        <div className="w-full my-6 px-6 md:px-5 lg:px-37.5">
+      <div className="w-full my-6 px-6 md:px-5 lg:px-37.5">
         <Breadcrumb>
           <BreadcrumbList className="flex items-center text-sm md:text-base lg:text-lg font-semibold">
             <BreadcrumbItem>
-              <BreadcrumbLink href="/take-action">
-                Take Action
-              </BreadcrumbLink>
+              <BreadcrumbLink href="/take-action">Take Action</BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbSeparator
               className={"[&>svg]:size-5 md:[&>svg]:size-6 lg:[&>svg]:size-7"}
@@ -33,15 +38,14 @@ const TakeActionDetailPage = () => {
           </BreadcrumbList>
         </Breadcrumb>
       </div>
-      <section className='w-full my-6 px-6 md:px-20 lg:px-37.5'>
-        <TakeActionDetailCard/>
+      <section className="w-full my-6 px-6 md:px-20 lg:px-37.5">
+        <TakeActionDetailCard />
       </section>
-      <section className='w-full my-6 px-6 md:px-20 lg:px-37.5 mt-12 mb-24'>
-        <TakeActionFormComponent/>
+      <section className="w-full my-6 px-6 md:px-20 lg:px-37.5 mt-12 mb-24">
+        <CreateTakeActionFormComponent />
       </section>
-      
     </main>
-  )
-}
+  );
+};
 
-export default TakeActionDetailPage
+export default TakeActionDetailPage;
