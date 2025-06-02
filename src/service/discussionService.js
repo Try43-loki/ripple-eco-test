1cf0ae12ef643eae8f59e3818157ecf4b06c29fc
Try43-loki <meysoytry@gmail.com>
2025-06-02 08:33:41 +0700

    fetch api

--- src/service/discussionService.js
+++ src/service/discussionService.js
@@ -1,7 +1,8 @@
+const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}`;
 export const getAllDiscussionsService = async () => {
   try {
     const res = await fetch(
-      "http://34.101.52.71:8882/api/v1/discussions/149152e2-8b86-440e-84fa-0e504adabc9b"
+      `${API_BASE_URL}/discussions/149152e2-8b86-440e-84fa-0e504adabc9b`
     );
     const data = await res.json();
     console.log("data", data);
